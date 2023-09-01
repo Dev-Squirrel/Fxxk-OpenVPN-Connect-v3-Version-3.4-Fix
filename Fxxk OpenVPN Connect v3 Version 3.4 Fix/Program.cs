82698743aa78e1f0f3f47041fea21ed8777825e4
Dev-Squirrel <youdienet@gmail.com>
2023-09-01 16:42:42 +0900

    초기 커밋.

---
+++ Fxxk OpenVPN Connect v3 Version 3.4 Fix/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter source folder path: ");
+        string sourceFolderPath = Console.ReadLine();
+
+        Console.Write("Enter destination folder path: ");
+        string destinationFolderPath = Console.ReadLine();
+
+        string[] ovpnFiles = Directory.GetFiles(sourceFolderPath, "*.ovpn");
+
+        foreach (string ovpnFile in ovpnFiles)
+        {
+            ProcessAndSaveOvpnFile(ovpnFile, destinationFolderPath);
+        }
+
+        Console.WriteLine("Processing and saving completed.");
+    }
+
+    static void ProcessAndSaveOvpnFile(string sourceFilePath, string destinationFolderPath)
+    {
+        string[] lines = File.ReadAllLines(sourceFilePath);
+
+        string[] unsupportedOptions = {
+            "resolv-retry", "persist-key", "persist-tun",
+            "tun-mtu-extra", "pull", "block-outside-dns"
+        };
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i].Trim();
+
+            if (IsUnsupportedOptionLine(line, unsupportedOptions))
+            {
+                lines[i] = "#" + lines[i];
+            }
+        }
+
+        string originalFileName = Path.GetFileNameWithoutExtension(sourceFilePath);
+        string destinationFileName = $"{originalFileName}_Version_3.4.ovpn";
+        string destinationFilePath = Path.Combine(destinationFolderPath, destinationFileName);
+
+        File.WriteAllLines(destinationFilePath, lines);
+        Console.WriteLine($"Processed and saved: {destinationFilePath}");
+    }
+
+    static bool IsUnsupportedOptionLine(string line, string[] unsupportedOptions)
+    {
+        foreach (string option in unsupportedOptions)
+        {
+            if (line.StartsWith(option))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Fxxk OpenVPN Connect v3 Version 3.4 Fix
+## 이게 뭐야?
+OpenVPN Connect v3 Version 3.4 버전부터 생긴 고통 받는 문제를 해결합니다.  
+https://forums.openvpn.net/viewtopic.php?t=35937  
+option_error: sorry, unsupported options present in configuration: UNKNOWN/UNSUPPORTED OPTION  
